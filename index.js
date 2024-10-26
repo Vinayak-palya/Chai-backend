69bd052663c6bd76c49b8f802aee292788eca6ec
Vinayak-Palya <vinayakpalya2704@gmail.com>
2024-10-26 10:15:29 +0530

    github data added

--- index.js
+++ index.js
@@ -2,7 +2,41 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4000
-
+const gitHubData = {
+    "login": "Vinayak-palya",
+    "id": 179369595,
+    "node_id": "U_kgDOCrD2ew",
+    "avatar_url": "https://avatars.githubusercontent.com/u/179369595?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Vinayak-palya",
+    "html_url": "https://github.com/Vinayak-palya",
+    "followers_url": "https://api.github.com/users/Vinayak-palya/followers",
+    "following_url": "https://api.github.com/users/Vinayak-palya/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Vinayak-palya/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Vinayak-palya/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Vinayak-palya/subscriptions",
+    "organizations_url": "https://api.github.com/users/Vinayak-palya/orgs",
+    "repos_url": "https://api.github.com/users/Vinayak-palya/repos",
+    "events_url": "https://api.github.com/users/Vinayak-palya/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Vinayak-palya/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 3,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2024-08-25T07:04:06Z",
+    "updated_at": "2024-10-09T14:59:59Z"
+  }
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -14,6 +48,9 @@ app.get('/login',(req , res) => {
 })
 app.get('/youtube' , (req , res) => {
     res.send("<h2> chai aur code </h2>")
+})
+app.get('/github' ,(req , res) => {
+    res.json(gitHubData)
 })
 
 app.listen(process.env.PORT, () => {
